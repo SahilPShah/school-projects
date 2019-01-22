532c07bb86d9598c5d9349c9bd1ae1d6609f8579
Sahil Shah <sahil.p.shah5@gmail.com>
2019-01-22 15:23:45 -0600

    removing 391

--- ece391/mp0/README.md
+++
@@ -1,3 +0,0 @@
-
-
-This is the mp0 repo for ECE 391. MP0 sets up the development and testing virtual machines that will be used throughout the class
--- ece391/mp3/student-distrib/tests.h
+++
@@ -1,8 +0,0 @@
-#ifndef TESTS_H
-#define TESTS_H
-
-// test launcher
-void launch_tests();
-extern int test_execute_asm(void);
-
-#endif /* TESTS_H */
